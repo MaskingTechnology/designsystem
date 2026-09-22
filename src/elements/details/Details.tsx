12219b48695e651dac6d7f54b37c9bd5ab6834a8
Peter van Vliet <peter@masking.tech>
2026-09-22 13:45:16 +0200

    #28: implement toggling hook
    Including rendering control

--- src/elements/details/Details.tsx
+++ src/elements/details/Details.tsx
@@ -1,5 +1,6 @@
 
-import type { ReactNode } from 'react';
+import { useState } from 'react';
+import type { ReactNode, ToggleEvent } from 'react';
 
 import './Details.css';
 
@@ -8,12 +9,26 @@ type Props = {
     readonly name?: string;
     readonly summary: ReactNode;
     readonly children: ReactNode;
+    readonly onToggle?: (open: boolean) => void;
 };
 
-export function Details({ open = false, name, summary, children }: Props)
+export function Details({ open = false, name, summary, children, onToggle }: Props)
 {
-    return <details className='details' open={open} name={name}>
+    const [opened, setOpened] = useState(open);
+    
+    const handleToggle = (event: ToggleEvent<HTMLDetailsElement>): void =>
+    {
+        const newState = event.currentTarget.open;
+
+        setOpened(newState);
+
+        if (onToggle === undefined) return;
+
+        onToggle(newState);
+    };
+
+    return <details className='details' open={open} name={name} onToggle={handleToggle} >
         <summary className='summary'>{summary}</summary>
-        <div className='content'>{children}</div>
+        <div className='content'>{opened && children}</div>
     </details>;
 }
